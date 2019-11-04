50cd37490dacd39a9b7c6857316f5feb4010f054
liangchangyou <liangchangyoujackson@gmail.com>
2019-11-05 00:00:24 +0800

    stack

--- src/com/liang/leetcode/SolutionEvalRPN.java
+++ src/com/liang/leetcode/SolutionEvalRPN.java
@@ -3,9 +3,9 @@ package com.liang.leetcode;
 import java.util.Stack;
 
 /**
- *  https://github.com/MisterBooo/LeetCodeAnimation/blob/master/notes/LeetCode%E7%AC%AC150%E5%8F%B7%E9%97%AE%E9%A2%98%EF%BC%9A%E9%80%86%E6%B3%A2%E5%85%B0%E8%A1%A8%E8%BE%BE%E5%BC%8F%E6%B1%82%E5%80%BC.md
- *
- *  https://leetcode-cn.com/problems/evaluate-reverse-polish-notation/
+ * https://github.com/MisterBooo/LeetCodeAnimation/blob/master/notes/LeetCode%E7%AC%AC150%E5%8F%B7%E9%97%AE%E9%A2%98%EF%BC%9A%E9%80%86%E6%B3%A2%E5%85%B0%E8%A1%A8%E8%BE%BE%E5%BC%8F%E6%B1%82%E5%80%BC.md
+ * <p>
+ * https://leetcode-cn.com/problems/evaluate-reverse-polish-notation/
  */
 public class SolutionEvalRPN {
     public int evalRPN(String[] tokens) {
